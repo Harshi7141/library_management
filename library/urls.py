829e2e7a9c7d905242e3e89ce3627b5dc2cefb61
Harshi7141 <harshikeshshinde823@gmail.com>
2025-07-27 20:59:05 +0530

    library management

---
+++ library/urls.py
@@ -0,0 +1,19 @@
+
+
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView
+from django.conf.urls import include
+
+
+urlpatterns = [
+    path('', views.home_view),
+    path('adminclick', views.adminclick_view),
+    path('adminsignup', views.adminsignup_view),
+    path('adminlogin', LoginView.as_view(template_name='library/adminlogin.html')),
+    path('addbook', views.addbook_view),
+    path('viewbook', views.viewbook_view),
+    path('aboutus', views.aboutus_view),
+    path('contactus', views.contactus_view),
+    
+]
